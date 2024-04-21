05658b358781b3cfbd1d44a57ec41e2fb6219f8c
awb99 <awb99@gmail.com>
2024-04-21 15:34:07 -0500

    dep bumps

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
    ))
 
 (def lib 'org.pinkgorilla/goldly-docs)
-(def version (format "0.5.%s" (b/git-count-revs nil)))
+(def version (format "0.7.%s" (b/git-count-revs nil)))
 
 (defn jar "build the JAR" [opts]
   (println "Building the JAR")
